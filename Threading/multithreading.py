6187633e87a171cabf4d2fd41afca41220e68d91
prathamesh-6099 <prathameshkolhe6099@gmail.com>
2026-01-18 19:38:24 +0530

    Threading and processing

--- Threading/multithreading.py
+++ Threading/multithreading.py
@@ -1,3 +1,7 @@
+### Multithreading
+## When to use Multi Threading
+###I/O-bound tasks: Tasks that spend more time waiting for I/O operations (e.g., file operations, network requests).
+###  Concurrent execution: When you want to improve the throughput of your application by performing multiple operations concurrently.
 import threading,time
 
 t=time.time()
